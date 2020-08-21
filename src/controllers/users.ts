1181c0c58f021c45b083a1c0d745f6b8d9b28ec9
Filipe <filipefernandes000@gmail.com>
2020-08-21 16:36:34 -0300

    add an abstract class to handle creation and update errors

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,14 +1,18 @@
 import { Post, Controller } from "@overnightjs/core";
 import { Request, Response } from "express";
 import { User } from "@src/models/user";
+import { BaseController } from ".";
 
 @Controller("users")
-export class UsersController {
+export class UsersController extends BaseController {
   @Post("")
   public async create(req: Request, res: Response): Promise<void> {
-    console.log("create");
-    const user = new User(req.body);
-    const newUser = await user.save();
-    res.status(201).send(newUser);
+    try {
+      const user = new User(req.body);
+      const newUser = await user.save();
+      res.status(201).send(newUser);
+    } catch (error) {
+      this.sendCreateUpdateErrorResponse(res, error);
+    }
   }
 }
