d4f07879b3ea847ac99c6c16cb4a1f96b9ed1168
Filipe <filipefernandes000@gmail.com>
2020-08-24 15:04:09 -0300

    Add encryption for users' passwords with bcrypt

--- test/functional/users.test.ts
+++ test/functional/users.test.ts
@@ -1,11 +1,12 @@
 import { User } from "@src/models/user";
+import AuthServices from "@src/services/auth";
 
 describe("Users functional test", () => {
   beforeEach(async () => {
     await User.deleteMany({});
   });
   describe("When creating a new user", () => {
-    it("should successfully create a new user", async () => {
+    it("should successfully create a new user with encrypted password", async () => {
       const newUser = {
         name: "John Doe",
         email: "john@mail.com",
@@ -13,8 +14,16 @@ describe("Users functional test", () => {
       };
       const response = await global.testRequest.post("/users").send(newUser);
 
+      await expect(
+        AuthServices.comparePasswords(newUser.password, response.body.password)
+      ).resolves.toBeTruthy();
       expect(response.status).toBe(201);
-      expect(response.body).toEqual(expect.objectContaining(newUser));
+      expect(response.body).toEqual(
+        expect.objectContaining({
+          ...newUser,
+          ...{ password: expect.any(String) },
+        })
+      );
     });
 
     it("should return 422 when there is a validation error", async () => {
