dea76274bd580c376906a15cd42003a963adc79a
Filipe <filipefernandes000@gmail.com>
2020-08-24 15:41:05 -0300

    Add authentication with jsonwebtoken

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,7 +1,9 @@
 import { Post, Controller } from "@overnightjs/core";
 import { Request, Response } from "express";
+import jwt from "jsonwebtoken";
 import { User } from "@src/models/user";
 import { BaseController } from ".";
+import AuthService from "@src/services/auth";
 
 @Controller("users")
 export class UsersController extends BaseController {
@@ -14,5 +16,32 @@ export class UsersController extends BaseController {
     } catch (error) {
       this.sendCreateUpdateErrorResponse(res, error);
     }
+  }
+
+  @Post("authenticate")
+  public async authenticate(req: Request, res: Response): Promise<Response> {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(401).send({
+        code: 401,
+        error: "User not found",
+      });
+    }
+
+    const isPasswordsEqual = await AuthService.comparePasswords(
+      password,
+      user.password
+    );
+    if (!isPasswordsEqual) {
+      return res.status(401).send({
+        code: 401,
+        error: "Password does not match!",
+      });
+    }
+
+    const token = AuthService.generateToken(user.toJSON());
+    return res.status(200).send({ token });
   }
 }
