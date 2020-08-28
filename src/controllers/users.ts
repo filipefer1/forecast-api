91a5d64ea9257da091a7443d8aa6af2f840f05d3
Filipe <filipefernandes000@gmail.com>
2020-08-28 16:31:28 -0300

    Add endpoint to decode token

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,9 +1,10 @@
-import { Post, Controller } from "@overnightjs/core";
+import { Post, Controller, Get, Middleware } from "@overnightjs/core";
 import { Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import { User } from "@src/models/user";
 import { BaseController } from ".";
 import AuthService from "@src/services/auth";
+import { authMiddleware } from "@src/middlewares/auth";
 
 @Controller("users")
 export class UsersController extends BaseController {
@@ -43,5 +44,19 @@ export class UsersController extends BaseController {
 
     const token = AuthService.generateToken(user.toJSON());
     return res.status(200).send({ ...user.toJSON(), ...{ token } });
+  }
+
+  @Get("me")
+  @Middleware(authMiddleware)
+  public async me(req: Request, res: Response): Promise<Response> {
+    const email = req.decoded ? req.decoded.email : undefined;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return this.sendErrorResponse(res, {
+        code: 404,
+        message: "User not found!",
+      });
+    }
+    return res.send({ user });
   }
 }
--- test/functional/users.test.ts
+++ test/functional/users.test.ts
@@ -96,4 +96,38 @@ describe("Users functional test", () => {
       expect(response.status).toBe(401);
     });
   });
+
+  describe("When getting user profille info", () => {
+    it("should return the token's owner profile information", async () => {
+      const newUser = {
+        name: "John Doe",
+        email: "john@mail.com",
+        password: "123456",
+      };
+      const user = await new User(newUser).save();
+      const token = AuthServices.generateToken(user.toJSON());
+      const { body, status } = await global.testRequest
+        .get("/users/me")
+        .set({ "x-access-token": token });
+
+      expect(status).toBe(200);
+      expect(body).toMatchObject(JSON.parse(JSON.stringify({ user })));
+    });
+
+    it("should return not found when the user is not found", async () => {
+      const newUser = {
+        name: "John Doe",
+        email: "john@mail.com",
+        password: "123456",
+      };
+      const user = await new User(newUser);
+      const token = AuthServices.generateToken(user.toJSON());
+      const { body, status } = await global.testRequest
+        .get("/users/me")
+        .set({ "x-access-token": token });
+
+      expect(status).toBe(404);
+      expect(body.message).toBe("User not found!");
+    });
+  });
 });
